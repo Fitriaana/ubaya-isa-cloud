cdbbb959c946f6088180f5bc421be77eecf16555
Fitriaana <80700633+Fitriaana@users.noreply.github.com>
2021-03-15 23:11:40 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome To My Page</title>
+	<style type="text/css">
+		h1{font-size: 15px; color: blue; font-weight: bold;}
+	</style>
+</head>
+<body>
+	<h1>Hello World!</h1>
+</body>
+</html>
